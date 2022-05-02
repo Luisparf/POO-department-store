e5a080ba5ebf6d5dc191fc03e80d3e9a0abe96bb
luisparf <luispauloaraujoafonso@gmail.com>
2022-05-02 15:03:34 -0300

    att

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,18 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -35,6 +42,20 @@ public class MainViewController implements Initializable {
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erros loading view", e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 
